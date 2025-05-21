82f42f716edd03fcce34b6e2aa56743508af8ab0
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-05-21 10:55:23 +0100

    Added govuk template to UI

--- src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTest {
+class BulkUploadApplicationTest {
     @Test
     void contextLoads() {
         // empty due to only testing context load
---
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/controller/BulkUploadControllerTest.java
@@ -0,0 +1,44 @@
+package uk.gov.justice.laa.cwa.bulkupload.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(BulkUploadController.class)
+class BulkUploadControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnUploadPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pages/upload"))
+                .andExpect(content().string(containsString("Select a file to upload")));
+
+    }
+
+    @Test
+    void shouldUploadFile() throws Exception {
+        mockMvc.perform(multipart("/upload").file(new MockMultipartFile("fileUpload", "test.txt", "text/plain", "test".getBytes())))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pages/upload-success"));
+    }
+
+    @Test
+    void shouldRejectEmptyFile() throws Exception {
+        mockMvc.perform(multipart("/upload").file(new MockMultipartFile("fileUpload", "empty.txt", "text/plain", new byte[0])))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pages/upload-failure"));
+    }
+}
