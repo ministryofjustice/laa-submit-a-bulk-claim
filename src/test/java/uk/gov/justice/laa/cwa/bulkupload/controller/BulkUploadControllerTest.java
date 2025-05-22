aecb8096a353bed248514b5dece9300be0fc4fa6
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-05-22 15:52:14 +0100

    Tidy up

--- src/test/java/uk/gov/justice/laa/cwa/bulkupload/controller/BulkUploadControllerTest.java
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/controller/BulkUploadControllerTest.java
@@ -2,7 +2,6 @@ package uk.gov.justice.laa.cwa.bulkupload.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
