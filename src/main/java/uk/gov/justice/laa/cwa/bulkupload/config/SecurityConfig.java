aa70dd03078eb42a4f3bf39d3801283878396cc3
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-05-22 14:20:02 +0100

    Added deploy workflow

--- src/main/java/uk/gov/justice/laa/cwa/bulkupload/config/SecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/cwa/bulkupload/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Profile("!test")
+@Profile("!test") // disable security for test profile
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
