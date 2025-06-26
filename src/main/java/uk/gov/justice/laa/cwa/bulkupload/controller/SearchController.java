1ed15508ff1da7285faf0fc173666197beaed34f
Solomanik <solomon.manikyam@justice.gov.uk>
2025-06-26 16:59:24 +0100

    retain search term on error (#30)

--- src/main/java/uk/gov/justice/laa/cwa/bulkupload/controller/SearchController.java
+++ src/main/java/uk/gov/justice/laa/cwa/bulkupload/controller/SearchController.java
@@ -46,7 +46,7 @@ public class SearchController {
             errors.put("searchTerm", "File reference must be between 1 to 10 characters long");
         }
         if (!errors.isEmpty()) {
-            return handleError(model, principal, provider, errors);
+            return handleError(model, principal, provider, searchTerm, errors);
         }
 
         List<CwaUploadSummaryResponseDto> summary;
@@ -56,7 +56,7 @@ public class SearchController {
         } catch (Exception e) {
             log.error("Error retrieving upload summary: {}", e.getMessage());
             errors.put("search", "Search failed please try again.");
-            return handleError(model, principal, provider, errors);
+            return handleError(model, principal, provider, searchTerm, errors);
         }
 
         try {
@@ -65,7 +65,7 @@ public class SearchController {
         } catch (Exception e) {
             log.error("Error retrieving upload errors: {}", e.getMessage());
             errors.put("search", "Search failed please try again.");
-            return handleError(model, principal, provider, errors);
+            return handleError(model, principal, provider, searchTerm, errors);
         }
 
         return "pages/submission-results";
@@ -74,14 +74,15 @@ public class SearchController {
     /**
      * Handles errors during the search process and prepares the model for rendering the upload page.
      *
-     * @param model     the model to add attributes to
-     * @param principal the authenticated user principal
-     * @param provider  the selected provider
-     * @param errors    a map of error messages
+     * @param model      the model to add attributes to
+     * @param principal  the authenticated user principal
+     * @param provider   the selected provider
+     * @param searchTerm the search term (file reference)
+     * @param errors     a map of error messages
      * @return the name of the view to render
      */
 
-    private String handleError(Model model, Principal principal, String provider, Map<String, String> errors) {
+    private String handleError(Model model, Principal principal, String provider, String searchTerm, Map<String, String> errors) {
         model.addAttribute("errors", errors);
         if (StringUtils.hasText(provider)) {
             try {
@@ -89,6 +90,9 @@ public class SearchController {
             } catch (NumberFormatException ignored) {
                 model.addAttribute("selectedProvider", 0);
             }
+        }
+        if (StringUtils.hasText(searchTerm)) {
+            model.addAttribute("searchTerm", searchTerm);
         }
         providerHelper.populateProviders(model, principal);
         model.addAttribute("tab", "search");
