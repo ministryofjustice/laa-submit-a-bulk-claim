9356f0b881aba0371b3735aac8b5e14782774157
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-07-18 14:03:08 +0100

    Fixed duplicate ID and checkstyle issues

--- src/main/java/uk/gov/justice/laa/cwa/bulkupload/config/ApplicationConfig.java
+++ src/main/java/uk/gov/justice/laa/cwa/bulkupload/config/ApplicationConfig.java
@@ -5,16 +5,28 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.LiteWebJarsResourceResolver;
 
+/**
+ * Adds application config regarding webjar locations.
+ *
+ * @author Jamie Briggs
+ */
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry
-        .addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/")
-        .resourceChain(true)
-        .addResolver(new LiteWebJarsResourceResolver());
-  }
+    /**
+     * Configures resource handling for the application. This method maps the "/webjars/**" URL
+     * pattern to the webjars resources located in the "classpath:/META-INF/resources/webjars/"
+     * directory and configures a resource chain with a custom resolver.
+     *
+     * @param registry the ResourceHandlerRegistry to register resource handlers
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/")
+            .resourceChain(true)
+            .addResolver(new LiteWebJarsResourceResolver());
+    }
 
 }
