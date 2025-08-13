cece38cf87156059a982e2f41b39ef9c8124e263
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-08-13 10:44:33 +0100

    Added mockup pages

---
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/MockupController.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.bulkclaim.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Mock up controller, not for production. This controller is used to mock up the UI for mockup
+ * purposes.
+ *
+ * @author Jamie Briggs
+ */
+@Controller
+@Profile("local")
+public class MockupController {
+
+  @GetMapping("/import-successful")
+  public String importComplete() {
+    return "pages/_import-successful";
+  }
+
+  @GetMapping("/import-failed")
+  public String importFailed() {
+    return "pages/_import-failed";
+  }
+
+  @GetMapping("/submission")
+  public String viewSubmission() {
+    return "pages/_submission";
+  }
+
+  @GetMapping("/claim-search")
+  public String search() {
+    return "pages/_search-results";
+  }
+}
