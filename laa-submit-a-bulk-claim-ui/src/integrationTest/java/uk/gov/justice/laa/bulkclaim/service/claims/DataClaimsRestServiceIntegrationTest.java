08ad4ade364397b491a61bc90dc0a1b2712c53d4
Jamie Briggs <personal@jbriggs.dev>
2025-08-28 09:35:18 +0100

    Fixes issues with matter starts integration tests (#13)
    * Fixes issues with matter starts integration tests
    
    * Spotless applied

--- laa-submit-a-bulk-claim-ui/src/integrationTest/java/uk/gov/justice/laa/bulkclaim/service/claims/DataClaimsRestServiceIntegrationTest.java
+++ laa-submit-a-bulk-claim-ui/src/integrationTest/java/uk/gov/justice/laa/bulkclaim/service/claims/DataClaimsRestServiceIntegrationTest.java
@@ -29,7 +29,7 @@ import uk.gov.justice.laa.claims.model.ClaimFields;
 import uk.gov.justice.laa.claims.model.ClaimValidationError;
 import uk.gov.justice.laa.claims.model.CreateBulkSubmission201Response;
 import uk.gov.justice.laa.claims.model.GetSubmission200Response;
-import uk.gov.justice.laa.claims.model.MatterStartsFields;
+import uk.gov.justice.laa.claims.model.MatterStartsGet;
 
 /**
  * Integration tests for the {@link DataClaimsRestService}.
@@ -401,7 +401,7 @@ class DataClaimsRestServiceIntegrationTest extends MockServerIntegrationTest {
                   .withHeader("Content-Type", "application/json")
                   .withBody(expectJson));
       // Then
-      MatterStartsFields block =
+      MatterStartsGet block =
           dataClaimsRestService.getSubmissionMatterStarts(submissionId, matterStartsId).block();
       String result = objectMapper.writeValueAsString(block);
       assertThatJsonMatches(expectJson, result);
