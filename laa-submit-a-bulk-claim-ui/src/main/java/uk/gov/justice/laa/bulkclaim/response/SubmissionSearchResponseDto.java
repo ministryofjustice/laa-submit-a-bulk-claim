6532122b7af531e3ddcf4f786db7b025031c05d0
vlorun-laa <victor.olorunleye1@justice.gov.uk>
2025-09-03 07:07:38 +0100

    removed unused models

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/response/SubmissionSearchResponseDto.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.laa.bulkclaim.response;
-
-import java.util.List;
-
-/** Submissions search response containing related claims. */
-public record SubmissionSearchResponseDto(List<SubmissionDto> submissions) {}
