8265d536d2195c1839d2c301fb8336e77b8a1fe1
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-10-07 17:32:42 +0100

    BC-136 | Turned print button into reusable component following progressive enhancement.

--- laa-submit-a-bulk-claim-ui/src/main/resources/static/javascripts/printButton.js
+++ laa-submit-a-bulk-claim-ui/src/main/resources/static/javascripts/printButton.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const printButton = document.getElementById('print-button')
-    if (printButton) {
-        printButton.addEventListener('click', function () {
-            window.print()
-        })
-    }
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const laaPrintButton = document.querySelector(
+      '[data-component="laa-print-button"]')
+  if (laaPrintButton) {
+    const printButtonSection = `
+        <div class="govuk-grid-column-one-half govuk-!-text-align-right">
+          <button type="button" class="govuk-button govuk-button--secondary"
+                  data-module="govuk-button" id="print-button"
+                  onclick="window.print()">
+            Print this page
+          </button>
+        </div>
+    `
+    laaPrintButton.classList = 'govuk-grid-column-one-half';
+    laaPrintButton.insertAdjacentHTML('afterend', printButtonSection);
+  }
+});
