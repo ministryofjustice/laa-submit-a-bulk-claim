a1b9c35508d70ac4b998acc82693ae02fc4a0d81
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-10-07 17:50:05 +0100

    BC-75 | Added new JS component which hides elements of the page for 2 second when the print button is clicked

--- laa-submit-a-bulk-claim-ui/src/main/resources/static/javascripts/printButton.js
+++ laa-submit-a-bulk-claim-ui/src/main/resources/static/javascripts/printButton.js
@@ -1,17 +1,36 @@
+const printPage = () => {
+  // Hide content on screen
+  let elements = document.querySelectorAll(
+      '[data-module="laa-hide-on-print-button"]');
+
+  elements.forEach(element => {
+    const originalDisplay = element.style.display;
+    element.style.display = 'none';
+    setTimeout(function () {
+      element.style.display = originalDisplay;
+    }, 2000);
+  });
+
+  window.print();
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const laaPrintButton = document.querySelector(
-      '[data-component="laa-print-button"]')
+      '[data-module="laa-print-button"]')
   if (laaPrintButton) {
     const printButtonSection = `
         <div class="govuk-grid-column-one-half govuk-!-text-align-right">
           <button type="button" class="govuk-button govuk-button--secondary"
-                  data-module="govuk-button" id="print-button"
-                  onclick="window.print()">
+                  data-module="govuk-button" id="print-button">
             Print this page
           </button>
         </div>
     `
     laaPrintButton.classList = 'govuk-grid-column-one-half';
     laaPrintButton.insertAdjacentHTML('afterend', printButtonSection);
+    document.getElementById('print-button').addEventListener('click',
+        printPage);
   }
+  
+  
 });
