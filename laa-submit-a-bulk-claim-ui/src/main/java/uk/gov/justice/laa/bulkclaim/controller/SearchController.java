b9dfca5a56a25a6881b609141580b683ecd97aa0
vlorun-laa <victor.olorunleye1@justice.gov.uk>
2025-10-15 10:38:34 +0100

    Story/bc 127 from and to date on the search submission calendar are disabled for the current day (#44)
    * initial update to templates and adds new validation rules, small refactor to controller
    
    * small refactor to search validation
    
    * small refactor to max date creation
    
    * amends submissionId validation

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/SearchController.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/SearchController.java
@@ -49,8 +49,6 @@ public class SearchController {
   private final OidcAttributeUtils oidcAttributeUtils;
 
   public static final String SUBMISSION_SEARCH_FORM = "submissionsSearchForm";
-  private static final DateTimeFormatter DATE_TIME_FORMATTER =
-      DateTimeFormatter.ofPattern("d/M/yyyy");
   private static final int DEFAULT_PAGE = 0;
   private static final int DEFAULT_PAGE_SIZE = 10;
 
@@ -189,7 +187,7 @@ public class SearchController {
       return null;
     }
     try {
-      return LocalDate.parse(date.trim(), DATE_TIME_FORMATTER);
+      return LocalDate.parse(date.trim(), DateTimeFormatter.ofPattern("d/M/yyyy"));
     } catch (DateTimeParseException exception) {
       log.warn("Unable to parse submitted date '{}': {}", date, exception.getMessage());
       return null;
