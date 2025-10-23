0c4737fbee42dc2d6c9d943db70f2bdf087591c2
Jamie Briggs <personal@jbriggs.dev>
2025-10-23 09:46:49 +0100

    BC-192 | Persist search filter (#75)
    * BC-192 | Introduced ThymeleafHrefUtils to help simplify building href attributes with multiple conditional request parameters
    
    * BC-192 | Added submissionId, submittedDateFrom, and submittedDateTo params to pagination
    
    * BC-192 | Added pageSort, removed unused sorting fragment
    
    * BC-192 | Fixes CVE-2025-11226

---
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/util/ThymeleafHrefUtils.java
@@ -0,0 +1,43 @@
+package uk.gov.justice.laa.bulkclaim.util;
+
+import java.util.StringJoiner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Utility class for building URLs with query parameters.
+ *
+ * @author Jamie Briggs
+ */
+@Component
+public class ThymeleafHrefUtils {
+
+  /**
+   * Builds a URL with query parameters. Will not add the query parameter if the parameter value is
+   * null or empty.
+   *
+   * @param baseUrl The base URL to append to.
+   * @param params The key-value pairs of query parameters.
+   * @return The built URL.
+   */
+  public String build(String baseUrl, String... params) {
+    if (baseUrl == null || baseUrl.isEmpty()) {
+      throw new IllegalArgumentException("Base URL cannot be null or empty");
+    }
+    if (params.length % 2 != 0) {
+      throw new IllegalArgumentException("Parameters should be provided in key-value pairs");
+    }
+
+    StringJoiner queryJoiner = new StringJoiner("&");
+    for (int i = 0; i < params.length; i += 2) {
+      String key = params[i];
+      String value = params[i + 1];
+
+      if (value != null && !value.isEmpty()) {
+        queryJoiner.add(key + "=" + value);
+      }
+    }
+
+    String queryString = queryJoiner.toString();
+    return queryString.isEmpty() ? baseUrl : baseUrl + "?" + queryString;
+  }
+}
