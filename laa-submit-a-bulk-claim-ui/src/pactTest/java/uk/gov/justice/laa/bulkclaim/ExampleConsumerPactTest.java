7e34d3fdaa0609e4789fd7ab11e05ea86ea7199c
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-01-08 15:19:53 +0000

    BC-109 | Got a working consumer pact test

--- laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/ExampleConsumerPactTest.java
+++ laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/ExampleConsumerPactTest.java
@@ -2,14 +2,16 @@ package uk.gov.justice.laa.bulkclaim;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import au.com.dius.pact.consumer.dsl.PactBuilder;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit.MockServerConfig;
 import au.com.dius.pact.consumer.junit5.PactConsumerTest;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.UUID;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
@@ -19,18 +21,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import reactor.core.publisher.Mono;
 import uk.gov.justice.laa.bulkclaim.client.DataClaimsRestClient;
 import uk.gov.justice.laa.bulkclaim.config.ClaimsApiPactTestConfig;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.SubmissionResponse;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {"claims-api.url=http://localhost:1234"})
 @PactConsumerTest
 @PactTestFor(providerName = "laa-data-claim-api")
+@MockServerConfig(port = "1234") // Same as Claims API URL port
 @Import(ClaimsApiPactTestConfig.class)
 public class ExampleConsumerPactTest {
 
-  final UUID SUBMISSION_ID = UUID.fromString("12345678-1234-1234-1234-123456789012");
+  final UUID SUBMISSION_ID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
 
   @Autowired
   DataClaimsRestClient dataClaimsRestClient;
@@ -40,23 +43,20 @@ public class ExampleConsumerPactTest {
 
   @SneakyThrows
   @Pact(consumer = "submission-consumer")
-  public V4Pact getSubmission(PactBuilder builder) {
+  public RequestResponsePact getSubmission(PactDslWithProvider builder) {
     String submissionResponse = readJsonFromFile("get-submission-200.json");
-
     return builder
-        .comment("V4 Pact using expectsToReceiveHttpInteraction")
-        .given("A submission exists")
-        .expectsToReceiveHttpInteraction(
-            "a request for a submission", http -> http
-                .withRequest(request -> request
-                    .path("/api/v0/submissions/" + SUBMISSION_ID)
-                    .method("GET"))
-                .willRespondWith(response -> response
-                    .status(200)
-                    .body(submissionResponse))
-        )
+        .given("a submission exists")
+        .uponReceiving("a request for a submission")
+        .path("/api/v0/submissions/" + SUBMISSION_ID)
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .headers(Map.of("Content-Type", "application/json"))
+        .body(submissionResponse)
         .toPact();
   }
+
 
   @Test
   @DisplayName("Verify get submission")
