7f93637e746160f87343c3e9bf490fe6e53fa7e9
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-01-09 12:18:28 +0000

    BC-109 | Added /api/v0/submissions consumer pact tests

--- laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/AbstractPactTest.java
+++ laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/AbstractPactTest.java
@@ -3,12 +3,20 @@ package uk.gov.justice.laa.bulkclaim;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 public abstract class AbstractPactTest {
   public static final String CONSUMER = "laa-submit-a-bulk-claim";
   public static final String PROVIDER = "laa-data-claim-api";
+
+  public static final String UUID_REGEX = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+
+  protected final List<String> userOffices = List.of("ABC123", "XYZ789");
+  protected final UUID submissionId = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+
 
   @MockitoBean
   OAuth2AuthorizedClientManager authorizedClientManager;
