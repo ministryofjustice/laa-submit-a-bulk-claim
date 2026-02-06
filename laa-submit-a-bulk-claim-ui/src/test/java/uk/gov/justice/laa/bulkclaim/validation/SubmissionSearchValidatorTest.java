3cd14cbd1acebc4702242206953eed48e75842c0
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-02-06 19:21:26 +0000

    BC-426 | Added validation to SubmissionSearchValidator

--- laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/validation/SubmissionSearchValidatorTest.java
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/validation/SubmissionSearchValidatorTest.java
@@ -4,20 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static uk.gov.justice.laa.bulkclaim.validation.SubmissionSearchValidator.AREA_OF_LAW;
 import static uk.gov.justice.laa.bulkclaim.validation.SubmissionSearchValidator.SUBMISSION_PERIOD;
+import static uk.gov.justice.laa.bulkclaim.validation.SubmissionSearchValidator.SUBMISSION_STATUS;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import uk.gov.justice.laa.bulkclaim.dto.SubmissionsSearchForm;
 import uk.gov.justice.laa.bulkclaim.util.SubmissionPeriodUtil;
+import uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw;
+import uk.gov.justice.laa.dstew.payments.claimsdata.model.SubmissionStatus;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Submission search validator test")
@@ -32,47 +40,158 @@ class SubmissionSearchValidatorTest {
     validator = new SubmissionSearchValidator(submissionPeriodUtil);
   }
 
-  @Test
-  @DisplayName("Should reject submissionPeriod when not available")
-  void validateShouldNotAcceptSubmissionPeriodNotAvailable() {
-    final SubmissionsSearchForm form =
-        SubmissionsSearchForm.builder().submissionPeriod("APR-2025").build();
-    final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+  @Nested
+  @DisplayName("Validate submissionPeriod")
+  class ValidateSubmissionPeriod {
 
-    when(submissionPeriodUtil.getAllPossibleSubmissionPeriods()).thenReturn(new LinkedHashMap<>());
+    @Test
+    @DisplayName("Should reject submissionPeriod when not available")
+    void validateShouldNotAcceptSubmissionPeriodNotAvailable() {
+      final SubmissionsSearchForm form =
+          SubmissionsSearchForm.builder().submissionPeriod("APR-2025").build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
 
-    validator.validate(form, errors);
+      when(submissionPeriodUtil.getAllPossibleSubmissionPeriods())
+          .thenReturn(new LinkedHashMap<>());
 
-    assertTrue(errors.hasFieldErrors(SUBMISSION_PERIOD));
-    assertEquals(
-        "search.error.submissionPeriod.invalid", errors.getFieldError(SUBMISSION_PERIOD).getCode());
+      validator.validate(form, errors);
+
+      assertTrue(errors.hasFieldErrors(SUBMISSION_PERIOD));
+      assertEquals(
+          "search.error.submissionPeriod.invalid",
+          errors.getFieldError(SUBMISSION_PERIOD).getCode());
+    }
+
+    @Test
+    @DisplayName("Should accept submissionPeriod when empty")
+    void shouldAcceptSubmissionPeriodWhenEmpty() {
+      final SubmissionsSearchForm form = SubmissionsSearchForm.builder().build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      when(submissionPeriodUtil.getAllPossibleSubmissionPeriods())
+          .thenReturn(new LinkedHashMap<>());
+
+      validator.validate(form, errors);
+
+      assertFalse(errors.hasFieldErrors(SUBMISSION_PERIOD));
+    }
+
+    @Test
+    @DisplayName("Should accept submissionPeriod when available")
+    void shouldAcceptSubmissionPeriodWhenAvailable() {
+      final SubmissionsSearchForm form =
+          SubmissionsSearchForm.builder().submissionPeriod("APR-2025").build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      when(submissionPeriodUtil.getAllPossibleSubmissionPeriods())
+          .thenReturn(Map.of("APR-2025", "April 2025"));
+
+      validator.validate(form, errors);
+
+      assertFalse(errors.hasFieldErrors(SUBMISSION_PERIOD));
+    }
   }
 
-  @Test
-  @DisplayName("Should accept submissionPeriod when empty")
-  void shouldAcceptSubmissionPeriodWhenEmpty() {
-    final SubmissionsSearchForm form = SubmissionsSearchForm.builder().build();
-    final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+  @Nested
+  @DisplayName("Validate areaOfLaw")
+  class ValidateAreaOfLaw {
 
-    when(submissionPeriodUtil.getAllPossibleSubmissionPeriods()).thenReturn(new LinkedHashMap<>());
+    @ParameterizedTest
+    @EnumSource(AreaOfLaw.class)
+    @DisplayName("Should have no errors when valid areaOfLaw")
+    void shouldHaveNoErrorsWhenValidAreaOfLaw(AreaOfLaw areaOfLaw) {
+      // Given
+      SubmissionsSearchForm form =
+          SubmissionsSearchForm.builder().areaOfLaw(areaOfLaw.getValue()).build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
 
-    validator.validate(form, errors);
+      // When
+      validator.validate(form, errors);
+      // Then
+      assertFalse(errors.hasFieldErrors(AREA_OF_LAW));
+    }
 
-    assertFalse(errors.hasFieldErrors(SUBMISSION_PERIOD));
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Should have no errors when areaOfLaw empty")
+    void shouldHaveNoErrorsWhenAreaOfLawEmpty(String areaOfLaw) {
+      // Given
+      SubmissionsSearchForm form = SubmissionsSearchForm.builder().areaOfLaw(areaOfLaw).build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      // When
+      validator.validate(form, errors);
+
+      // Then
+      assertFalse(errors.hasFieldErrors(AREA_OF_LAW));
+    }
+
+    @Test
+    @DisplayName("Should have error when invalid value")
+    void shouldHaveErrorWhenInvalidValue() {
+      // Given
+      SubmissionsSearchForm form = SubmissionsSearchForm.builder().areaOfLaw("ABC").build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      // When
+      validator.validate(form, errors);
+
+      // Then
+      assertTrue(errors.hasFieldErrors(AREA_OF_LAW));
+      assertEquals("search.error.areaOfLaw.invalid", errors.getFieldError(AREA_OF_LAW).getCode());
+    }
   }
 
-  @Test
-  @DisplayName("Should accept submissionPeriod when available")
-  void shouldAcceptSubmissionPeriodWhenAvailable() {
-    final SubmissionsSearchForm form =
-        SubmissionsSearchForm.builder().submissionPeriod("APR-2025").build();
-    final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+  @Nested
+  @DisplayName("Validate submission status")
+  class ValidateSubmissionStatus {
 
-    when(submissionPeriodUtil.getAllPossibleSubmissionPeriods())
-        .thenReturn(Map.of("APR-2025", "April 2025"));
+    @ParameterizedTest
+    @EnumSource(SubmissionStatus.class)
+    @DisplayName("Should have no errors when valid submission status")
+    void shouldHaveNoErrorsWhenValidSubmissionStatus(SubmissionStatus submissionStatus) {
+      // Given
+      SubmissionsSearchForm form =
+          SubmissionsSearchForm.builder().submissionStatus(submissionStatus.getValue()).build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
 
-    validator.validate(form, errors);
+      // When
+      validator.validate(form, errors);
+      // Then
+      assertFalse(errors.hasFieldErrors(SUBMISSION_STATUS));
+    }
 
-    assertFalse(errors.hasFieldErrors(SUBMISSION_PERIOD));
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("Should have no errors when submission status empty")
+    void shouldHaveNoErrorsWhenSubmissionStatusEmpty(String submissionStatus) {
+      // Given
+      SubmissionsSearchForm form =
+          SubmissionsSearchForm.builder().submissionStatus(submissionStatus).build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      // When
+      validator.validate(form, errors);
+
+      // Then
+      assertFalse(errors.hasFieldErrors(SUBMISSION_STATUS));
+    }
+
+    @Test
+    @DisplayName("Should have error when invalid value")
+    void shouldHaveErrorWhenInvalidValue() {
+      // Given
+      SubmissionsSearchForm form = SubmissionsSearchForm.builder().submissionStatus("ABC").build();
+      final BindingResult errors = new BeanPropertyBindingResult(form, "submissionsSearchForm");
+
+      // When
+      validator.validate(form, errors);
+
+      // Then
+      assertTrue(errors.hasFieldErrors(SUBMISSION_STATUS));
+      assertEquals(
+          "search.error.submissionStatus.invalid",
+          errors.getFieldError(SUBMISSION_STATUS).getCode());
+    }
   }
 }
