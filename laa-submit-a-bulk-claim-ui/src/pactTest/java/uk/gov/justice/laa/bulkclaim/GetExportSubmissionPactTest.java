d58de853beda32c699707b7d3d8738a83bfce6e3
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-02-25 15:56:45 +0000

    BC-475 | Added crime lower and mediation export checks

--- laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/GetExportSubmissionPactTest.java
+++ laa-submit-a-bulk-claim-ui/src/pactTest/java/uk/gov/justice/laa/bulkclaim/GetExportSubmissionPactTest.java
@@ -2,22 +2,19 @@ package uk.gov.justice.laa.bulkclaim;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import au.com.dius.pact.consumer.dsl.PactDslRootValue;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit.MockServerConfig;
 import au.com.dius.pact.consumer.junit5.PactConsumerTest;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.regex.Pattern;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import uk.gov.justice.laa.bulkclaim.client.ExportDataClaimsRestClient;
 import uk.gov.justice.laa.bulkclaim.config.ClaimsApiPactTestConfig;
@@ -29,8 +26,8 @@ import uk.gov.justice.laa.bulkclaim.config.ClaimsApiPactTestConfig;
  * then verify itself against the generated pact to ensure it remains compatible with it's
  * consumers.
  *
- * <p>For the various {@link Pact} annotations, a scenario is created. There are multiple parts of a
- * {@link RequestResponsePact}:
+ * <p>For the various {@link Pact} annotations, a scenario is created. There are multiple parts of
+ * a {@link RequestResponsePact}:
  *
  * <ul>
  *   <li>Given: This explains the state of what the Claims API should be in when expecting this
@@ -54,7 +51,8 @@ import uk.gov.justice.laa.bulkclaim.config.ClaimsApiPactTestConfig;
 @DisplayName("GET: /exports/submission-claims-{area-of-law} PACT tests")
 public final class GetExportSubmissionPactTest extends AbstractPactTest {
 
-  @Autowired ExportDataClaimsRestClient exportDataClaimsRestClient;
+  @Autowired
+  ExportDataClaimsRestClient exportDataClaimsRestClient;
 
   String legalHelpCsvHeaders =
       "Providers LAA Office Number,Submission Month,Area of Law,Legal Help Submission Reference,"
@@ -95,17 +93,57 @@ public final class GetExportSubmissionPactTest extends AbstractPactTest {
           + "Interview Fee,Calculated Fee Detail - Bolt On Substantive Hearing Fee,Calculated Fee"
           + " Detail - Escape Case Flag\n";
 
-  @SneakyThrows
+  String crimeLowerCsvHeaders =
+      "Providers LAA Office Number,Submission Month,Area of Law,Crime Lower Schedule Number,Stage"
+          + " Reached (Claim Code),Fee Code,Claim Status,Client surname,Client Initial,Gender,"
+          + "Ethnicity,Disability,UFN,Representation Order Date,Standard Fee Category,Outcome for"
+          + " the client,Matter Type,Net Profit costs (excluding VAT),Net Disbursement amount "
+          + "(excluding VAT),Net Travel costs (excluding VAT),Net Waiting costs (excluding VAT),"
+          + "VAT Indicator,VAT Disbursements amount,Case concluded date,No. of suspects/ "
+          + "defendants,No. of Police Station/court attendances,Police Station/ Court ID/ Prison "
+          + "ID,DSCC Number,MAAT ID,Prison law Prior Approval number,Duty Solicitor,Youth Court,"
+          + "Scheme ID,Calculated Fee Detail - Fee Code Description,Calculated Fee Detail - Fee "
+          + "Type,Calculated Fee Detail - Category of Law,Calculated Fee Detail - Total Amount,"
+          + "Calculated Fee Detail - VAT Indicator,Calculated Fee Detail - VAT Rate Applied,"
+          + "Calculated Fee Detail - Calculated VAT Amount,Calculated Fee Detail - Disbursement "
+          + "Amount,Calculated Fee Detail - Requested Net Disbursement Amount,Calculated Fee "
+          + "Detail - Disbursement VAT Amount,Calculated Fee Detail - Hourly Total Amount,"
+          + "Calculated Fee Detail - Fixed Fee Amount,Calculated Fee Detail - Net Profit Costs "
+          + "Amount,Calculated Fee Detail - Requested Net Profit Costs Amount,Calculated Fee "
+          + "Detail - Net Travel Costs Amount,Calculated Fee Detail - Net Waiting Costs Amount,"
+          + "Calculated Fee Detail - Travel And Waiting Costs Amount,Calculated Fee Detail - "
+          + "Escape Case Flag\n";
+
+  String mediationCsvHeaders =
+      "Providers LAA Office Number,Submission Month,Area of Law,Mediation Submission Reference,"
+          + "Matter Type (1:2),Fee Code,Case Reference Number,Case Start Date,Claim ID,Unique "
+          + "Case ID,Claim Status,Case Concluded Date,Client 1 Forename,Client 1 Surname,Client 1"
+          + " Date of Birth,Client 1 UCN,Client 1 Postcode,Client 1 Gender,Client 1 Ethnicity,"
+          + "Client 1 Disability,Client 1 Legally Aided?,Client 2 Forename,Client 2 Surname,"
+          + "Client 2 Date of Birth,Client 2 UCN,Client 2 Postcode,Client 2 Gender,Client 2 "
+          + "Ethnicity,Client 2 Disability,Client 2 Legally Aided?,Number of Mediation Sessions,"
+          + "Mediation Time (mins),Outcome,Outreach Location,Referral,VAT Indicator,Net "
+          + "Disbursement Amount (Excluding VAT),Disbursements VAT amount,Client 1 Postal "
+          + "Application accepted,Client 2 Postal Application accepted,Schedule Reference "
+          + "(Outcome),Calculated Fee Detail - Fee Code Description,Calculated Fee Detail - Fee "
+          + "Type,Calculated Fee Detail - Category of Law,Calculated Fee Detail - Total Amount,"
+          + "Calculated Fee Detail - VAT Indicator,Calculated Fee Detail - VAT Rate Applied,"
+          + "Calculated Fee Detail - Calculated VAT Amount,Calculated Fee Detail - Disbursement "
+          + "Amount,Calculated Fee Detail - Requested Net Disbursement Amount,Calculated Fee "
+          + "Detail - Disbursement VAT Amount,Calculated Fee Detail - Fixed Fee Amount,Calculated"
+          + " Fee Detail - Net Profit Costs Amount,Calculated Fee Detail - Requested Net Profit "
+          + "Costs Amount,Calculated Fee Detail - Net Travel Costs Amount,Calculated Fee Detail -"
+          + " Net Waiting Costs Amount,Calculated Fee Detail - Travel And Waiting Costs Amount\n";
+
   @Pact(consumer = CONSUMER)
   public RequestResponsePact getLegalHelpSubmission200(PactDslWithProvider builder) {
-
     // Must match exactly the header row, optionally followed by newline
     String exampleBody = legalHelpCsvHeaders;
 
     // Defines expected 200 response for existing submission
     return builder
         .given("no data exists for the export")
-        .uponReceiving("a request to export a submission")
+        .uponReceiving("a request to export a legal help submission")
         .matchPath("/exports/submission-claims-legal-help")
         .matchQuery("submission-id", UUID_REGEX)
         .matchQuery("office", ANY_FORMAT_REGEX)
@@ -117,14 +155,80 @@ public final class GetExportSubmissionPactTest extends AbstractPactTest {
         .toPact();
   }
 
+  @Pact(consumer = CONSUMER)
+  public RequestResponsePact getCrimeLowerSubmission200(PactDslWithProvider builder) {
+    // Must match exactly the header row, optionally followed by newline
+    String exampleBody = crimeLowerCsvHeaders;
+
+    // Defines expected 200 response for existing submission
+    return builder
+        .given("no data exists for the export")
+        .uponReceiving("a request to export a crime lower submission")
+        .matchPath("/exports/submission-claims-crime-lower")
+        .matchQuery("submission-id", UUID_REGEX)
+        .matchQuery("office", ANY_FORMAT_REGEX)
+        .matchHeader(HttpHeaders.AUTHORIZATION, UUID_REGEX)
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .body(exampleBody)
+        .toPact();
+  }
+
+  @Pact(consumer = CONSUMER)
+  public RequestResponsePact getMediationSubmission200(PactDslWithProvider builder) {
+    // Must match exactly the header row, optionally followed by newline
+    String exampleBody = mediationCsvHeaders;
+
+    // Defines expected 200 response for existing submission
+    return builder
+        .given("no data exists for the export")
+        .uponReceiving("a request to export a mediation submission")
+        .matchPath("/exports/submission-claims-mediation")
+        .matchQuery("submission-id", UUID_REGEX)
+        .matchQuery("office", ANY_FORMAT_REGEX)
+        .matchHeader(HttpHeaders.AUTHORIZATION, UUID_REGEX)
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .body(exampleBody)
+        .toPact();
+  }
+
   @Test
-  @DisplayName("Verify 200 response")
+  @DisplayName("Verify 200 response - Legal Help")
   @PactTestFor(pactMethod = "getLegalHelpSubmission200")
-  void verify200Response() {
+  void verifyLegalHelp200Response() {
     byte[] csvData =
         exportDataClaimsRestClient
             .getSubmissionExport("legal-help", SUBMISSION_ID, "testOffice")
-            .map(response -> response.getBody())
+            .map(HttpEntity::getBody)
+            .block();
+
+    assertThat(csvData).isNotNull();
+  }
+
+  @Test
+  @DisplayName("Verify 200 response - Crime Lower")
+  @PactTestFor(pactMethod = "getCrimeLowerSubmission200")
+  void verifyCrimeLower200Response() {
+    byte[] csvData =
+        exportDataClaimsRestClient
+            .getSubmissionExport("crime-lower", SUBMISSION_ID, "testOffice")
+            .map(HttpEntity::getBody)
+            .block();
+
+    assertThat(csvData).isNotNull();
+  }
+
+  @Test
+  @DisplayName("Verify 200 response - Mediation")
+  @PactTestFor(pactMethod = "getMediationSubmission200")
+  void verifyMediation200Response() {
+    byte[] csvData =
+        exportDataClaimsRestClient
+            .getSubmissionExport("mediation", SUBMISSION_ID, "testOffice")
+            .map(HttpEntity::getBody)
             .block();
 
     assertThat(csvData).isNotNull();
