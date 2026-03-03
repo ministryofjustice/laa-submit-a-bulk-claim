fa7b7a0c0b0793df62e9b2c0e9a8467da38e9a20
Jamie Briggs <personal@jbriggs.dev>
2026-03-03 12:17:03 +0000

    BC-475 | Export submission claims (#168)
    * BC-475 | Updated printButton.js to support pre-defined action containers. Updated padding on summary.html to make action buttons and H1 align corectly.
    
    * BC-475 | Added ExportDataClaimsRestClient for downloading exports from Claims API, and ExportSubmissionDetailController to expose the downloaded file to the frontend.
    
    * BC-475 | Updated ExportSubmissionDetailController to use DataClaimsRestClient for more secure file creation, and unit tests added
    
    * BC-475 | Added initial Pact test for GetExportSubmissionPactTest
    
    * BC-475 | Spotless applied
    
    * BC-475 | Updated ExportDataClaimsRestClient to use updated request param name submisison-id. Also update pact test to use regex from CSV headers
    
    * BC-475 | Updated Pact to match CSV content exactly
    
    * BC-475 | Added crime lower and mediation export checks
    
    * BC-475 | Spotless applied
    
    * BC-475 | Updated build.gradle
    
    * BC-475 | Forwarded headers from Claims API for file download in ExportSubmissionDetailController
    
    * BC-475 | Updates to ExportSubmissionDetailController to include request parameters to avoid additional call to Claims API. Fixes to existing tests following Claims API models upgrade.
    
    * BC-475 | Removed unused import, and bumped spring boot commons to 2.1.0
    
    * BC-475 | Updated models package to latest claims API version
    
    * BC-475 | Updated summary.html to hide export button when submission is invalid.
    
    * BC-475 | Added office check to ensure users can't manipulate export button to export different office
    
    * BC-475 | Added additional attributes to ExportSubmissionDetailController to include content type and content length
    
    * BC-475 | Upgrade dependencies
    
    * BC-475 | Removed content length attribute from ExportSubmissionDetailController
    
    * BC-475 | Only add headers which are required
    
    * BC-475 | Updated export button text to be "Download Claims"
    
    * BC-475 | Page layout adjustments

---
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/ExportSubmissionDetailController.java
@@ -0,0 +1,76 @@
+package uk.gov.justice.laa.bulkclaim.controller;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+import uk.gov.justice.laa.bulkclaim.client.ExportDataClaimsRestClient;
+import uk.gov.justice.laa.bulkclaim.exception.SubmitBulkClaimException;
+import uk.gov.justice.laa.bulkclaim.util.OidcAttributeUtils;
+
+/**
+ * Controller responsible for exporting submission details as a CSV file. This class provides an
+ * endpoint to trigger the export of submission data for a specified area of law and office.
+ *
+ * @author Jamie Briggs
+ */
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ExportSubmissionDetailController {
+
+  private final ExportDataClaimsRestClient exportDataClaimsRestClient;
+  private final OidcAttributeUtils oidcAttributeUtils;
+
+  /**
+   * Exports the submission details as a CSV file for a specified submission ID, office, and area of
+   * law. The generated file is sent as a downloadable resource in the response.
+   *
+   * @param submissionId the unique identifier of the submission to be exported
+   * @param office the office account number for the submission
+   * @param areaOfLaw the area of law for the submission
+   * @return a {@code Mono} of {@code ResponseEntity} containing a downloadable {@code Resource} of
+   *     the CSV export
+   */
+  @GetMapping("/submission/{submissionId}/export")
+  public Mono<ResponseEntity<Resource>> exportSubmissionDetail(
+      @PathVariable UUID submissionId,
+      @RequestParam String office,
+      @RequestParam String areaOfLaw,
+      @AuthenticationPrincipal OidcUser oidcUser) {
+    var offices = oidcAttributeUtils.getUserOffices(oidcUser);
+    if (!offices.contains(office)) {
+      throw new SubmitBulkClaimException(
+          "User (%s) does not have access to office: %s"
+              .formatted(oidcUser.getPreferredUsername(), office));
+    }
+
+    String areaOfLawPathVariable = areaOfLaw.toLowerCase().replace(" ", "-");
+
+    Mono<ResponseEntity<byte[]>> submissionExport =
+        exportDataClaimsRestClient.getSubmissionExport(areaOfLawPathVariable, submissionId, office);
+
+    return submissionExport.map(
+        file -> {
+          // Only add headers we need (Spring automatically adds some headers so don't want
+          // to duplicate this)
+          HttpHeaders safeHeaders = new HttpHeaders();
+          safeHeaders.setContentType(file.getHeaders().getContentType());
+          safeHeaders.setContentDisposition(file.getHeaders().getContentDisposition());
+
+          return ResponseEntity.ok()
+              .headers(safeHeaders)
+              .body(new ByteArrayResource(file.getBody()));
+        });
+  }
+}
