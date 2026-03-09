2d81a992eafb6b256f0e71fcad470c10f6b46420
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-03-09 17:11:41 +0000

    remove changes

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/BulkUploadBeingCheckedController.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/BulkUploadBeingCheckedController.java
@@ -6,7 +6,6 @@ import static uk.gov.justice.laa.bulkclaim.constants.SessionConstants.UPLOADED_F
 
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
@@ -52,12 +51,8 @@ public class BulkUploadBeingCheckedController {
     SubmissionResponse submission;
 
     try {
-      TimeUnit.SECONDS.sleep(3);
       submission = dataClaimsRestClient.getSubmission(submissionId).block();
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      throw new IllegalStateException("Interrupted while waiting before submission check", e);
-    } catch (WebClientResponseException e) {
+    }  catch (WebClientResponseException e) {
       if (e.getStatusCode().isSameCodeAs(HttpStatusCode.valueOf(404))) {
         log.debug("No submission found, will retry: %s".formatted(submissionId.toString()));
         model.addAttribute("shouldRefresh", true);
