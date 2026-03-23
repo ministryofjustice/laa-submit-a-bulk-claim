e2bf9f68c4011e4a46630fa4e918a6b4264c7ea9
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-03-23 19:52:15 +0000

    Spotless applied

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/validation/FileFirusValidator.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/validation/FileFirusValidator.java
@@ -2,6 +2,4 @@ package uk.gov.justice.laa.bulkclaim.validation;
 
 import org.springframework.validation.Validator;
 
-public interface FileFirusValidator extends Validator {
-
-}
+public interface FileFirusValidator extends Validator {}
