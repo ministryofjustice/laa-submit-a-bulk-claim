90c698a1ab01b363ab878ab50a5be146181f1753
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-04-08 09:59:04 +0100

    claimsummary javadoc

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/dto/submission/claim/ClaimSummary.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/dto/submission/claim/ClaimSummary.java
@@ -17,32 +17,35 @@ import lombok.Builder;
  * @param uniqueClientNumber the unique client number
  * @param client2Forename the second client's forename
  * @param client2Surname the second client's surname
+ * @param uniqueClientNumber2 the unique client number for client2
  * @param outcomeCode the outcome code
  * @param caseConcludedDate the case concluded date
  * @param officeAccountNumber the office account
  * @param standardFeeCategoryCode the category of law
  * @param matterType1 the matter type 1
  * @param matterType2 the matter type 2
+ * @param isVatApplicable the vat applicable flag
+ * @param caseStartDate the case start date
  * @param categoryOfLaw the category of law
  * @param feeCodeDescription the fee code description
  * @param submissionDate the date submitted
- * @param client2Name the client 2 name
  * @param clientName the client name
+ * @param client2Name the client 2 name
  * @param isEscaped case escaped flag
  */
 @Builder
 public record ClaimSummary(
     String areaOfLaw,
+    String uniqueFileNumber, /*CIVIL, CRIME*/
     String matterTypeCode, /*CRIME, MEDIATION*/
     String feeCode, /*CIVIL, MEDIATION*/
     String clientForename, /*CIVIL, CRIME, MEDIATION*/
     String clientSurname, /*CIVIL, CRIME, MEDIATION*/
+    String stageReachedCode, /*CRIME*/
     String uniqueClientNumber, /*CIVIL, MEDIATION*/
     String client2Forename, /*MEDIATION*/
     String client2Surname, /*MEDIATION*/
     String uniqueClientNumber2, /*CIVIL, MEDIATION*/
-    String stageReachedCode, /*CRIME*/
-    String uniqueFileNumber, /*CIVIL, CRIME*/
     String outcomeCode, /*CRIME*/
     LocalDate caseConcludedDate, /*CRIME*/
     String officeAccountNumber,
