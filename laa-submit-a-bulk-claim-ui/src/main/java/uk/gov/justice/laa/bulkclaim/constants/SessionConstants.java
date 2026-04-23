41fa3739c19e0e80e6fdee47077f4117f3581d35
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-04-23 10:04:04 +0100

    Remove unused code

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/constants/SessionConstants.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/constants/SessionConstants.java
@@ -10,11 +10,9 @@ public final class SessionConstants {
   private SessionConstants() {}
 
   public static final String BULK_SUBMISSION_ID = "bulkSubmissionId";
-  public static final String BULK_SUBMISSION = "bulkClaimSubmission";
 
   public static final String SUBMISSION_ID = "submissionId";
   public static final String SUBMISSION_DATE_TIME = "submissionDateTime";
   public static final String UPLOADED_FILENAME = "uploadedFilename";
-  public static final String SUBMISSION = "claimSubmission";
   public static final String CLAIM_ID = "claimId";
 }
