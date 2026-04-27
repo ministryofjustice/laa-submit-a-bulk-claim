ba5d3e81e944cdd3c8a15ff8314d7ae2539c3889
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-04-27 18:51:30 +0100

    add metrics

---
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/config/MetricsConfig.java
@@ -0,0 +1,19 @@
+package uk.gov.justice.laa.bulkclaim.config;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.justice.laa.bulkclaim.service.SessionService;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public Gauge activeSessionsGauge(MeterRegistry registry, SessionService sessionService) {
+        return Gauge.builder(
+                        "active.sessions.count", sessionService, SessionService::getActiveSessionCount)
+                .description("Number of active sessions")
+                .register(registry);
+    }
+}
