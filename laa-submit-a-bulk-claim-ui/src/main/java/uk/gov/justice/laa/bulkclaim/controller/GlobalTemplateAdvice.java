68659699ee16eaf4f72b254bcb20b15fc8f2849e
Phil Payne <phil.payne@digital.justice.gov.uk>
2026-04-27 10:50:20 +0100

    Revert and cntinue use of thymeleaf-extras-springsecurity6

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/GlobalTemplateAdvice.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/controller/GlobalTemplateAdvice.java
@@ -1,11 +1,8 @@
 package uk.gov.justice.laa.bulkclaim.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import uk.gov.justice.laa.bulkclaim.dto.AuthenticatedUserDetails;
 
 /**
  * A controller advice that provides global attributes to be used across the application. This class
@@ -35,44 +32,5 @@ public class GlobalTemplateAdvice {
   @ModelAttribute("laaBulkUploadGuidanceText")
   public String laaBulkUploadGuidanceText() {
     return laaBulkUploadGuidanceText;
-  }
-
-  /**
-   * Builds authenticated user details from authentication.
-   *
-   * @param authentication the authentication object
-   * @return the authenticated user details
-   */
-  @ModelAttribute("authenticatedUser")
-  public AuthenticatedUserDetails authenticatedUser(Authentication authentication) {
-    return new AuthenticatedUserDetails(
-        getAttribute(authentication, "preferred_username", "email"),
-        getAttribute(authentication, "FIRM_NAME", "firm_name"),
-        getAttribute(authentication, "FIRM_CODE", "firm_code"));
-  }
-
-  /**
-   * Builds authenticated user details from authentication.
-   *
-   * @param authentication the authentication object
-   * @return the authenticated user details
-   */
-  private String getAttribute(Authentication authentication, String... keys) {
-    if (authentication == null || authentication.getPrincipal() == null) {
-      return "";
-    }
-
-    Object principal = authentication.getPrincipal();
-
-    if (principal instanceof OAuth2User oauthUser) {
-      for (String key : keys) {
-        Object value = oauthUser.getAttributes().get(key);
-        if (value != null) {
-          return value.toString();
-        }
-      }
-    }
-
-    return authentication.getName() != null ? authentication.getName() : "";
   }
 }
