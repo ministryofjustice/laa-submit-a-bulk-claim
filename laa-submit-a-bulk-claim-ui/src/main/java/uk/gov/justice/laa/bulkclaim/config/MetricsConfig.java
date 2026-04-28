d8ec2439a0caf807fffb8b66a16ae97658aeb8fb
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-04-28 09:35:09 +0100

    checkstyle

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/config/MetricsConfig.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/config/MetricsConfig.java
@@ -6,11 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.justice.laa.bulkclaim.service.SessionService;
 
-/** Config class for metrics. */
+/**
+ * Config class for metrics.
+ * */
 @Configuration
 public class MetricsConfig {
 
   /**
+   * Creates a gauge that tracks the number of active sessions.
+   *
    * @param registry for the meter registry used to register the gauge
    * @param sessionService the service providing session count
    * @return a gauge representing active sessions
