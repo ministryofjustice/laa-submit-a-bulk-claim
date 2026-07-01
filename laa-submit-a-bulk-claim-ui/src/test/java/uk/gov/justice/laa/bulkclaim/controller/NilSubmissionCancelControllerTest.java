ed6157af0dce0157fd6c89d444a2ba9cc069fb1c
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-07-01 12:05:18 +0100

    cancel controller test

---
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/controller/NilSubmissionCancelControllerTest.java
@@ -0,0 +1,55 @@
+package uk.gov.justice.laa.bulkclaim.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uk.gov.justice.laa.bulkclaim.config.FeatureFlagsConfig;
+import uk.gov.justice.laa.bulkclaim.dto.submission.NilSubmissionForm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
+class NilSubmissionCancelControllerTest {
+
+    @Mock private FeatureFlagsConfig featureFlagsConfig;
+
+    @InjectMocks private NilSubmissionCancelController nilSubmissionCancelController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenFeatureFlagDisabled_shouldReturnErrorView(){
+        NilSubmissionForm form = new NilSubmissionForm();
+
+        when(featureFlagsConfig.getIsNilSubmissionEnabled()).thenReturn(false);
+
+        assertEquals("error", nilSubmissionCancelController.getCancel(form));
+
+        verify(featureFlagsConfig).getIsNilSubmissionEnabled();
+        verifyNoMoreInteractions(featureFlagsConfig);
+    }
+
+    @Test
+    void whenFeatureFlagEnabled_shouldRedirectToUpload() {
+        when(featureFlagsConfig.getIsNilSubmissionEnabled()).thenReturn(true);
+        NilSubmissionForm form = new NilSubmissionForm();
+
+        form.setOffice("office1");
+        form.setAreaOfLaw("areaOfLaw1");
+        form.setSubmissionPeriod("submissionPeriod1");
+        form.setScheduleReference("scheduleReference1");
+
+        nilSubmissionCancelController.getCancel(form);
+
+        assertNull(form.getOffice());
+        assertNull(form.getAreaOfLaw());
+        assertNull(form.getSubmissionPeriod());
+        assertNull(form.getScheduleReference());
+    }
+}
