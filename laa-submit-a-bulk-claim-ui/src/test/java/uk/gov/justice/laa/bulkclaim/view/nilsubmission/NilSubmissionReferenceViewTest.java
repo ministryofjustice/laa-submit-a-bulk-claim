c7055b649728a62cac7575d0e3495cd047841f7e
Michael Boyd <michael.boyd1@justice.gov.uk>
2026-07-27 12:38:18 +0100

    test(BC-704): Add view tests for reference

--- laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/view/nilsubmission/NilSubmissionReferenceViewTest.java
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/view/nilsubmission/NilSubmissionReferenceViewTest.java
@@ -1,13 +1,20 @@
 package uk.gov.justice.laa.bulkclaim.view.nilsubmission;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw.CRIME_LOWER;
+import static uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw.LEGAL_HELP;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw.MEDIATION;
 
+import java.util.stream.Stream;
 import org.jsoup.Jsoup;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -15,12 +22,53 @@ import uk.gov.justice.laa.bulkclaim.controller.ControllerTestHelper;
 import uk.gov.justice.laa.bulkclaim.controller.nilsubmission.NilSubmissionReferenceController;
 import uk.gov.justice.laa.bulkclaim.dto.submission.NilSubmissionForm;
 import uk.gov.justice.laa.bulkclaim.view.ViewTestBase;
+import uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw;
 
 @WebMvcTest(NilSubmissionReferenceController.class)
 class NilSubmissionReferenceViewTest extends ViewTestBase {
 
   NilSubmissionReferenceViewTest() {
     this.mapping = "/nil-submission/reference";
+  }
+
+  private static Stream<Arguments> referencePageContentArguments() {
+    return Stream.of(
+        of(CRIME_LOWER, "Crime lower", "CRM/0P322F/2025", "Crime schedule number"),
+        of(LEGAL_HELP, "Legal help", "0P322F/Civil/01", "Civil submission reference"),
+        of(MEDIATION, "Mediation", "0P322F/MEDI2024/01", "Mediation submission reference"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("referencePageContentArguments")
+  void referencePageContent(
+      AreaOfLaw areaOfLaw, String areaOfLawText, String exampleText, String labelText) {
+    NilSubmissionForm form = new NilSubmissionForm();
+    form.setOffice("0P322F");
+    form.setOfficeCount(2);
+    form.setAreaOfLaw(areaOfLaw);
+    form.setSubmissionPeriod("OCT-2025");
+    session.setAttribute("nilSubmissionForm", form);
+
+    var doc = renderDocument();
+
+    assertPageHasTitle(doc, "Create a nil submission");
+    assertPageHasBackLink(doc);
+
+    assertPageHasHint(doc, "nil-submission-hint", "Create a nil submission");
+    assertPageHasHeading(doc, "Add your submission reference");
+    assertPageHasHint(doc, "nil-submission-example-hint", exampleText);
+
+    var summaryList = getFirstSummaryList(doc);
+    assertThat(summaryList).hasSize(3);
+    assertSummaryListRowContainsValues(summaryList.get(0), "Office account number", "0P322F");
+    assertSummaryListRowContainsValues(summaryList.get(1), "Area of law", areaOfLawText);
+    assertSummaryListRowContainsValues(summaryList.get(2), "Submission period", "OCT-2025");
+
+    assertPageHasLabel(doc, "submissionReference-input", labelText);
+    assertThat(selectFirst(doc, "#submissionReference-input").attr("value")).isEmpty();
+
+    assertPageHasPrimaryButton(doc, "Continue");
+    assertPageHasSecondaryLink(doc, "Cancel");
   }
 
   @Test
