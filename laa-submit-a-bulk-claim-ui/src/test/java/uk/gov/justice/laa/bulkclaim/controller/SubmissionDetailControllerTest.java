09ff49cfa92bc90ba53a0a565609bf902a49c4c0
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-08-05 16:22:03 +0100

    Remove 'saveAsDraft' functionality from bulk claim submissions
    This removes the 'saveAsDraft' option from the file upload process, related controllers, services, and validations. The corresponding UI elements, DTOs, and tests were updated to reflect this change. Removed unused references, properties, and client calls associated with this functionality.

--- laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/controller/SubmissionDetailControllerTest.java
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/controller/SubmissionDetailControllerTest.java
@@ -124,8 +124,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   new BigDecimal("100.50"),
                   "Legal aid",
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
       when(submissionMessagesBuilder.build(any(), any(), any(), anyInt(), anyInt(), any()))
           .thenReturn(
               new MessagesSummary(Collections.emptyList(), 0, 0, pagination, MessagesSource.CLAIM));
@@ -170,8 +169,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   new BigDecimal("100.50"),
                   "Legal aid",
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
       when(submissionMessagesBuilder.buildErrors(any(), anyInt(), anyInt(), any()))
           .thenReturn(
               new MessagesSummary(Collections.emptyList(), 0, 0, pagination, MessagesSource.CLAIM));
@@ -211,8 +209,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   new BigDecimal("100.50"),
                   AreaOfLaw.LEGAL_HELP.getValue(),
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
       List<SubmissionMatterStartsRow> matterTypes = new ArrayList<>();
       matterTypes.add(new SubmissionMatterStartsRow("Description", 34));
       when(submissionClaimDetailsBuilder.build(any(), anyInt(), anyInt(), any()))
@@ -259,8 +256,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   null,
                   AreaOfLaw.CRIME_LOWER.getValue(),
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
       when(submissionClaimDetailsBuilder.build(any(), anyInt(), anyInt(), any()))
           .thenReturn(
               new SubmissionClaimsDetails(Collections.emptyList(), pagination, BigDecimal.TEN));
@@ -303,8 +299,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   BigDecimal.ONE,
                   AreaOfLaw.CRIME_LOWER.getValue(),
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
       when(submissionClaimDetailsBuilder.build(any(), anyInt(), anyInt(), any()))
           .thenReturn(
               new SubmissionClaimsDetails(
@@ -377,8 +372,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   new BigDecimal("100.50"),
                   AreaOfLaw.LEGAL_HELP.getValue(),
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
 
       var pagination = Page.builder().totalPages(1).totalElements(0).number(0).size(10).build();
       when(submissionClaimDetailsBuilder.build(any(), anyInt(), anyInt(), any()))
@@ -422,8 +416,7 @@ class SubmissionDetailControllerTest extends BaseControllerTest {
                   "AQ2B3C",
                   new BigDecimal("100.50"),
                   AreaOfLaw.LEGAL_HELP.getValue(),
-                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC),
-                  false));
+                  OffsetDateTime.of(2025, 1, 1, 10, 10, 10, 0, ZoneOffset.UTC)));
 
       var pagination = Page.builder().totalPages(1).totalElements(0).number(0).size(10).build();
       when(submissionClaimDetailsBuilder.build(any(), anyInt(), anyInt(), any()))
