d7fd5b0a120d6a7da8a0cafab4130048b9f4e1c0
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-08-06 15:03:34 +0100

    use bean

--- laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/util/DateTimeUtil.java
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/util/DateTimeUtil.java
@@ -3,12 +3,14 @@ package uk.gov.justice.laa.bulkclaim.util;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DateTimeUtil {
 
   private static final ZoneId LONDON_TIMEZONE = ZoneId.of("Europe/London");
 
-  public static LocalDateTime toLondonLocalDateTime(OffsetDateTime value) {
+  public LocalDateTime toLondonLocalDateTime(OffsetDateTime value) {
     return value.atZoneSameInstant(LONDON_TIMEZONE).toLocalDateTime();
   }
 }
--- laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/util/DateTimeUtilTest.java
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/bulkclaim/util/DateTimeUtilTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Date time util tests")
 class DateTimeUtilTest {
 
+  private final DateTimeUtil dateTimeUtil = new DateTimeUtil();
+
   @Test
   @DisplayName("Should convert UTC submitted time to London time during BST")
   void shouldConvertUtcToLondonLocalDateTime() {
     OffsetDateTime utcDateTime = OffsetDateTime.of(2025, 7, 1, 10, 10, 10, 0, ZoneOffset.UTC);
 
-    LocalDateTime result = DateTimeUtil.toLondonLocalDateTime(utcDateTime);
+    LocalDateTime result = dateTimeUtil.toLondonLocalDateTime(utcDateTime);
 
     assertThat(result).isEqualTo(LocalDateTime.of(2025, 7, 1, 11, 10, 10));
   }
@@ -26,7 +28,7 @@ class DateTimeUtilTest {
   void shouldConvertUtcToLondonLocalDateTimeInWinter() {
     OffsetDateTime utcDateTime = OffsetDateTime.of(2025, 1, 15, 10, 0, 0, 0, ZoneOffset.UTC);
 
-    LocalDateTime result = DateTimeUtil.toLondonLocalDateTime(utcDateTime);
+    LocalDateTime result = dateTimeUtil.toLondonLocalDateTime(utcDateTime);
 
     assertThat(result).isEqualTo(LocalDateTime.of(2025, 1, 15, 10, 0, 0));
   }
