28fc591c27de83a4e88bd7d485830addbef6d45e
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-08-06 14:17:17 +0100

    to London time

---
+++ laa-submit-a-bulk-claim-ui/src/main/java/uk/gov/justice/laa/bulkclaim/util/DateTimeUtil.java
@@ -0,0 +1,14 @@
+package uk.gov.justice.laa.bulkclaim.util;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+public class DateTimeUtil {
+
+  private static final ZoneId LONDON_TIMEZONE = ZoneId.of("Europe/London");
+
+  public static LocalDateTime toLondonLocalDateTime(OffsetDateTime value) {
+    return value.atZoneSameInstant(LONDON_TIMEZONE).toLocalDateTime();
+  }
+}
