06e21d1c2233fe830e7cecc83cdc0f80f00339f7
gabrielageorgieva <gabriela.georgieva@justice.gov.uk>
2026-08-06 17:07:12 +0100

    add tests for successful submission for all areas of law

--- e2e/src/main/java/uk/gov/justice/laa/bulkclaim/e2e/pages/UploadPage.java
+++ e2e/src/main/java/uk/gov/justice/laa/bulkclaim/e2e/pages/UploadPage.java
@@ -1,20 +1,34 @@
 package uk.gov.justice.laa.bulkclaim.e2e.pages;
 
+import static com.microsoft.playwright.options.AriaRole.BUTTON;
 import static com.microsoft.playwright.options.AriaRole.LINK;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import java.nio.file.Path;
 import lombok.Getter;
 
 @Getter
 public class UploadPage extends BasePage {
 
   private final Locator createNilSubmissionButton;
+  private final Locator continueButton;
+  private final Locator fileInput;
 
   public UploadPage(Page page) {
     super(page, "Upload a bulk claim file");
 
     createNilSubmissionButton =
         page.getByRole(LINK, new Page.GetByRoleOptions().setName("Create nil submission"));
+
+    continueButton =
+        page.getByRole(BUTTON, new Page.GetByRoleOptions().setName("Continue"));
+
+    fileInput = page.locator("#file-input");
+  }
+
+  public void uploadFile(Path filePath) {
+    page.onFileChooser(fileChooser -> fileChooser.setFiles(filePath));
+    page.locator(".govuk-drop-zone").click();
   }
 }
--- e2e/src/test/java/uk/gov/justice/laa/bulkclaim/e2e/tests/BulkSubmissionE2ETest.java
+++ e2e/src/test/java/uk/gov/justice/laa/bulkclaim/e2e/tests/BulkSubmissionE2ETest.java
@@ -1,17 +1,63 @@
 package uk.gov.justice.laa.bulkclaim.e2e.tests;
 
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import uk.gov.justice.laa.bulkclaim.e2e.base.BaseTest;
 import uk.gov.justice.laa.bulkclaim.e2e.pages.LandingPagePage;
+import uk.gov.justice.laa.bulkclaim.e2e.pages.SubmissionDetailPage;
+import uk.gov.justice.laa.bulkclaim.e2e.pages.UploadBeingCheckedPage;
 import uk.gov.justice.laa.bulkclaim.e2e.pages.UploadPage;
 
 public class BulkSubmissionE2ETest extends BaseTest {
 
   @Test
-  public void bulkSubmission() {
+  public void bulkSubmissionForCrimeLowerAccepted() {
     var landingPage = new LandingPagePage(page);
     landingPage.getStartNowButton().click();
 
     var upload = new UploadPage(page);
+    var csvPath = Paths.get("../docs/sample-data/crime-lower-may-2026.csv").toAbsolutePath();
+    upload.uploadFile(csvPath);
+    
+    upload.getContinueButton().click();
+    
+    var uploadBeingChecked = new UploadBeingCheckedPage(page);
+
+    var submissionDetailPage = new SubmissionDetailPage(page);
+    submissionDetailPage.assertSubmissionAccepted();
+  }
+
+  @Test
+  public void bulkSubmissionForLegalHelpAccepted() {
+    var landingPage = new LandingPagePage(page);
+    landingPage.getStartNowButton().click();
+
+    var upload = new UploadPage(page);
+    var csvPath = Paths.get("../docs/sample-data/legal-help-june-2026.csv").toAbsolutePath();
+    upload.uploadFile(csvPath);
+
+    upload.getContinueButton().click();
+
+    var uploadBeingChecked = new UploadBeingCheckedPage(page);
+
+    var submissionDetailPage = new SubmissionDetailPage(page);
+    submissionDetailPage.assertSubmissionAccepted();
+  }
+
+  @Test
+  public void bulkSubmissionForMediationAccepted() {
+    var landingPage = new LandingPagePage(page);
+    landingPage.getStartNowButton().click();
+
+    var upload = new UploadPage(page);
+    var csvPath = Paths.get("../docs/sample-data/mediation-june-2026.csv").toAbsolutePath();
+    upload.uploadFile(csvPath);
+
+    upload.getContinueButton().click();
+
+    var uploadBeingChecked = new UploadBeingCheckedPage(page);
+
+    var submissionDetailPage = new SubmissionDetailPage(page);
+    submissionDetailPage.assertSubmissionAccepted();
   }
 }
