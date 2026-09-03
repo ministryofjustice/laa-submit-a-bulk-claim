864aa9b131436b54a851d457ba4cd44594bbe840
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-09-03 09:55:12 +0100

    BC-679 | Removed duplication on area of law view tests as they're covered on SubmissionDetailsSummaryViewTest

--- laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/payments/submit/view/SubmissionDetailsLegalHelpViewTest.java
+++ laa-submit-a-bulk-claim-ui/src/test/java/uk/gov/justice/laa/payments/submit/view/SubmissionDetailsLegalHelpViewTest.java
@@ -119,9 +119,6 @@ class SubmissionDetailsLegalHelpViewTest extends SubmissionDetailsViewTestBase {
   void renderLegalHelpSubmissionDetails() {
     Document doc = renderDocument();
 
-    assertPageHasHeading(doc, "Submission summary");
-    assertThat(selectFirst(doc, ".govuk-tag--green").text()).isEqualTo("Accepted");
-
     // Summary
     var summaryList = getFirstSummaryList(doc);
     assertThat(summaryList).hasSize(6);
@@ -168,16 +165,6 @@ class SubmissionDetailsLegalHelpViewTest extends SubmissionDetailsViewTestBase {
     assertThat(selectFirst(rows.get(1), ".govuk-tag--yellow").text()).isEqualTo("Amended");
     assertThat(selectFirst(rows.get(1), "a").attr("href"))
         .contains("/submissions/%s/claims/%s".formatted(submissionId, FIXED_FEE_CLAIM_ID));
-
-    assertPageHasSecondaryButton(doc, "Download claims");
-    var exportButton = selectFirst(doc, "#export-button");
-    assertThat(exportButton.attr("href"))
-        .contains("/submissions/%s/export".formatted(submissionId))
-        .contains("office=%s".formatted(OFFICE_CODE))
-        .contains("areaOfLaw=");
-    assertThat(
-            selectFirst(doc, "[data-module=laa-print-button]").attr("data-print-action-container"))
-        .isEqualTo("secondary-action-container");
   }
 
   @Test
