269e31b59130279c393235e67a3918202abdf223
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-09-04 15:24:49 +0100

    BC-679 | Updated SubmissionDetailsE2ETest to include coverage for successful crime lower page

--- e2e/src/test/java/uk/gov/justice/laa/payments/submit/e2e/tests/SubmissionDetailsE2ETest.java
+++ e2e/src/test/java/uk/gov/justice/laa/payments/submit/e2e/tests/SubmissionDetailsE2ETest.java
@@ -1,6 +1,8 @@
 package uk.gov.justice.laa.payments.submit.e2e.tests;
 
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import uk.gov.justice.laa.payments.submit.e2e.base.BaseTest;
@@ -10,7 +12,7 @@ import uk.gov.justice.laa.payments.submit.e2e.pages.SubmissionDetailPage;
 import uk.gov.justice.laa.payments.submit.e2e.pages.UploadBeingCheckedPage;
 import uk.gov.justice.laa.payments.submit.e2e.pages.UploadPage;
 
-public class SubmissionDetailsE2ETest extends BaseTest {
+class SubmissionDetailsE2ETest extends BaseTest {
 
   @Test
   void legalHelpSubmissionSuccessful() {
@@ -46,9 +48,9 @@ public class SubmissionDetailsE2ETest extends BaseTest {
         "0P322F", "Legal help", "APR-2026", "£33,115.60");
 
     // Assert tabs are visible
-    submissionDetails.getClaimsTab().isVisible();
-    submissionDetails.getMessagesTab().isVisible();
-    submissionDetails.getMatterStartsTab().isVisible();
+    assertThat(submissionDetails.getClaimsTab()).isVisible();
+    assertThat(submissionDetails.getMessagesTab()).isVisible();
+    assertThat(submissionDetails.getMatterStartsTab()).isVisible();
 
     // Assert claims tab
     submissionDetails.assertTotalClaims(9);
@@ -60,5 +62,51 @@ public class SubmissionDetailsE2ETest extends BaseTest {
     // Assert matter starts tab
     submissionDetails.getMatterStartsTab().click();
     submissionDetails.assertTotalMatterStarts(2);
+  }
+
+  @Test
+  void crimeLowerSubmissionSuccessful() {
+    var landingPage = new LandingPagePage(page);
+    landingPage.getStartNowButton().click();
+
+    var uploadPage = new UploadPage(page);
+    var csvPath = Paths.get("../docs/sample-data/crime-lower-june-2026.csv").toAbsolutePath();
+    uploadPage.uploadFile(csvPath);
+    uploadPage.getContinueButton().click();
+
+    new UploadBeingCheckedPage(page);
+
+    var submissionDetailPage = new SubmissionDetailPage(page);
+
+    // Checks that submission details page is reached via upload. Will validate page content
+    //  after searching for submission.
+    submissionDetailPage.assertSubmissionAccepted();
+
+    // Next search for the same submission
+    uploadPage.getSearchLink().click();
+    var searchPage = new SearchPage(page);
+    searchPage.getSearchButton().click();
+
+    // View the newly submitted submission
+    searchPage.clickOnLink(0);
+
+    var submissionDetails = new SubmissionDetailPage(page);
+    // Assert basic summary details
+    submissionDetails.assertSubmissionAccepted();
+    submissionDetailPage.assertTotalWarnings(5);
+    submissionDetails.assertSubmissionSummary(
+        "0P322F", "Crime lower", "JUN-2026", "£5,465.47");
+
+    // Assert tabs are visible
+    assertThat(submissionDetails.getClaimsTab()).isVisible();
+    assertThat(submissionDetails.getMessagesTab()).isVisible();
+    assertThat(submissionDetails.getMatterStartsTab()).isHidden();
+
+    // Assert claims tab
+    submissionDetails.assertTotalClaims(10);
+
+    // Assert messages tab
+    submissionDetails.getMessagesTab().click();
+    submissionDetails.assertTotalMessages(5);
   }
 }
